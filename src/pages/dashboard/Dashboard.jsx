cb08ed29d94934921f8c6d8aefaa513d2d2ef191
milan vala <milanvala31@gmail.com>
2023-10-21 00:11:40 +0530

    fix

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,11 +4,9 @@ import { Table, TableCell, TableRow, Typography } from "@mui/material";
 import sampleData from "./sample-weather-data.json";
 
 const Dashboard = () => {
-  console.log("sample data =>", sampleData);
   const {
     response: { categoryAnalysisWeekly, weatherData },
   } = sampleData;
-  console.log("categoryAnalysisWeekly =>", categoryAnalysisWeekly);
 
   const customWeatherData = weatherData.slice(0, 7);
   const customCategoryAnalysisWeekly = categoryAnalysisWeekly.slice(0, 5);
@@ -21,10 +19,8 @@ const Dashboard = () => {
     }
     sortedList[categoryName].push(category);
   });
-  console.log("sortedList ==>", sortedList);
 
-  const categoryEntires = Object.entries(sortedList)
-  categoryEntires.forEach(([key, value]) => console.log(`${key}: ${value}`))
+  const categoryEntires = Object.entries(sortedList);
 
   return (
     <div style={{ width: "100" }}>
@@ -55,14 +51,6 @@ const Dashboard = () => {
                 </TableCell>
               );
             })}
-
-            {/* <TableCell>1/24</TableCell>
-            <TableCell>1/25</TableCell>
-            <TableCell>1/26</TableCell>
-            <TableCell>1/27</TableCell>
-            <TableCell>1/28</TableCell>
-            <TableCell>1/29</TableCell>
-            <TableCell>1/30</TableCell> */}
           </TableRow>
           <TableRow>
             <TableCell />
@@ -81,13 +69,6 @@ const Dashboard = () => {
               );
             })}
 
-            {/* <TableCell>Mon</TableCell>
-            <TableCell>Tue</TableCell>
-            <TableCell>Wed</TableCell>
-            <TableCell>Thu</TableCell>
-            <TableCell>Fri</TableCell>
-            <TableCell>Sat</TableCell>
-            <TableCell>Sun</TableCell> */}
             <TableCell>Sales % Total</TableCell>
           </TableRow>
           <TableRow>
@@ -103,12 +84,6 @@ const Dashboard = () => {
               );
             })}
 
-            {/* <TableCell>01/27-01/02</TableCell>
-            <TableCell>01/03-01/09</TableCell>
-            <TableCell>01/10-01/16</TableCell>
-            <TableCell>01/17-01/23</TableCell>
-            <TableCell>01/24-01/30</TableCell> */}
-
             {customWeatherData.slice(0, 7).map((data, index) => {
               return (
                 <TableCell key={index}>
@@ -117,13 +92,6 @@ const Dashboard = () => {
               );
             })}
 
-            {/* <TableCell>77'/44'</TableCell>
-            <TableCell>77'/44'</TableCell>
-            <TableCell>77'/44'</TableCell>
-            <TableCell>77'/44'</TableCell>
-            <TableCell>77'/44'</TableCell>
-            <TableCell>77'/44'</TableCell>
-            <TableCell>77'/44'</TableCell> */}
             <TableCell>WOs % Total</TableCell>
           </TableRow>
           <TableRow>
@@ -133,18 +101,6 @@ const Dashboard = () => {
             {customCategoryAnalysisWeekly.map((data, index) => {
               return <TableCell key={index}>{data.psaTotalSales}</TableCell>;
             })}
-
-            {/* <TableCell>41</TableCell>
-            <TableCell>7</TableCell>
-            <TableCell>10</TableCell>
-            <TableCell>12</TableCell>
-            <TableCell>7</TableCell> */}
-
-            {/* some othere data */}
-            <TableCell>10</TableCell>
-            <TableCell>10</TableCell>
-            <TableCell>10</TableCell>
-            <TableCell>100.0</TableCell>
           </TableRow>
           <TableRow>
             <TableCell>PSA Total</TableCell>
@@ -154,137 +110,35 @@ const Dashboard = () => {
                 <TableCell key={index}>{data.psaTotalWriteOffs}</TableCell>
               );
             })}
-
-            {/* <TableCell>1</TableCell>
-            <TableCell>1</TableCell>
-            <TableCell>2</TableCell>
-            <TableCell>1</TableCell>
-            <TableCell>1</TableCell> */}
-
-            {/* some othere data */}
-            <TableCell>10</TableCell>
-            <TableCell>10</TableCell>
-            <TableCell>10</TableCell>
-            <TableCell>100.0</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell />
-            <TableCell>Del</TableCell>
-            <TableCell>294</TableCell>
-            <TableCell>294</TableCell>
-            <TableCell>444</TableCell>
-            <TableCell>444</TableCell>
-            <TableCell>444</TableCell>
-            {/* some othere data */}
-            <TableCell>.</TableCell>
-            <TableCell>3</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell />
-            <TableCell>Sales</TableCell>
-            <TableCell>35</TableCell>
-            <TableCell>2</TableCell>
-            <TableCell>6</TableCell>
-            <TableCell>4</TableCell>
-            <TableCell>5</TableCell>
-            {/* some othere data */}
-            <TableCell>.</TableCell>
-            <TableCell>3</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
           </TableRow>
 
-
-        {/* {categoryEntires.forEach(([key, value]) => {
-          return (
-            <TableRow>
-              <TableCell>{key}</TableCell>
-              <TableCell>WOsss</TableCell>
-              <TableCell>{value}</TableCell>
-            </TableRow>
-          )
-        })} */}
-
-
-          <TableRow>
-            <TableCell>Baby food</TableCell>
-            <TableCell>WOs</TableCell>
-            <TableCell>0</TableCell>
-            <TableCell>0</TableCell>
-            <TableCell>0</TableCell>
-            <TableCell>0</TableCell>
-            <TableCell>1</TableCell>
-            {/* some othere data */}
-            <TableCell>.</TableCell>
-            <TableCell>0</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell />
-            <TableCell>Del</TableCell>
-            <TableCell>444</TableCell>
-            <TableCell>444</TableCell>
-            <TableCell>444</TableCell>
-            <TableCell>444</TableCell>
-            <TableCell>444</TableCell>
-            {/* some othere data */}
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-          </TableRow>
-
-
-
-          <TableRow>
-            <TableCell />
-            <TableCell>Sales</TableCell>
-            <TableCell>35</TableCell>
-            <TableCell>35</TableCell>
-            <TableCell>35</TableCell>
-            <TableCell>35</TableCell>
-            <TableCell>35</TableCell>
-            {/* some othere data */}
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Backing</TableCell>
-            <TableCell>WOs</TableCell>
-            <TableCell>1</TableCell>
-            <TableCell>1</TableCell>
-            <TableCell>1</TableCell>
-            <TableCell>1</TableCell>
-            <TableCell>1</TableCell>
-            {/* some othere data */}
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-            <TableCell>.</TableCell>
-          </TableRow>
+          {categoryEntires.map(([key, value]) => {
+            return (
+              <>
+                <TableRow>
+                  <TableCell />
+                  <TableCell>Del</TableCell>
+                  {value.map((item, index) => {
+                    return <TableCell key={index}>{item.deliveries}</TableCell>;
+                  })}
+                </TableRow>
+                <TableRow>
+                  <TableCell />
+                  <TableCell>Sales</TableCell>
+                  {value.map((item, index) => {
+                    return <TableCell key={index}>{item.sales}</TableCell>;
+                  })}
+                </TableRow>
+                <TableRow>
+                  <TableCell>{key}</TableCell>
+                  <TableCell>WO's</TableCell>
+                  {value.map((item, index) => {
+                    return <TableCell key={index}>{item.writeOffs}</TableCell>;
+                  })}
+                </TableRow>
+              </>
+            );
+          })}
         </Table>
       </div>
     </div>
